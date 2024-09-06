14bc32458d39bbb2d83ea76e2d1fedb6d1a70340
nighs <142889497+nky02@users.noreply.github.com>
2024-09-06 17:48:37 +0800

    Update User.java

--- OOPROG/JAVA_SLMS/User.java
+++ OOPROG/JAVA_SLMS/User.java
@@ -4,12 +4,50 @@ public class User {
     private String username;
     private String password;
     private String userType;
-
+    
     public User nextUser; // Linked list: points to next user
 
-    /*  
-        TODO add setters and getters for the private variables
-        Use "librarian" and "user" for user type
-    */
+    public User(String username, String password, String userType) {
+        this.username = username;
+        this.password = password;
+        setUserType(userType); // Ensure valid userType is set
+    }
 
+    // Getter and Setter for username
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter and Setter for password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Getter and Setter for userType
+    public String getUserType() {
+        return userType;
+    }
+
+    //conditions for user types
+    public void setUserType(String userType) {
+        if (userType.equalsIgnoreCase("librarian") || userType.equalsIgnoreCase("user")) {
+            this.userType = userType;
+        } else {
+            throw new IllegalArgumentException("User type must be either 'librarian' or 'user'");
+        }
+    }
+
+    // sisplay details/info
+    public void displayUser() {
+        System.out.println("Username: " + username);
+        System.out.println("User Type: " + userType);
+    }
 }
