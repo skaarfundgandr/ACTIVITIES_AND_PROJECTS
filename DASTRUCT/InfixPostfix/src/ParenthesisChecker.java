60be2dac82a4b2784279299b9ea131ec5a25b13f
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-10-03 21:49:37 +0800

    Added code for ParenthesisChecker in Java

---
+++ DASTRUCT/InfixPostfix/src/ParenthesisChecker.java
@@ -0,0 +1,31 @@
+public class ParenthesisChecker {
+    private Stack stack;
+
+    public ParenthesisChecker() {
+        stack = new Stack();
+    }
+
+    public boolean check(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (stack.head == null) {
+                stack.push(str.charAt(i));
+            } else if ((stack.peek() == '(') && (str.charAt(i) == ')')) {
+                stack.pop();
+            } else {
+                stack.push(str.charAt(i));
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        ParenthesisChecker parenthesisChecker = new ParenthesisChecker();
+
+        if (parenthesisChecker.check("(()))")) {
+            System.out.println("Valid expression");
+        } else {
+            System.out.println("Invalid expression");
+        }
+    }
+}
