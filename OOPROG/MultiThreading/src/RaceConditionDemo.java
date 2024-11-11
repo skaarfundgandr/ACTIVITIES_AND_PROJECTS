24e8ea4c311649230e2415cd91cfc94fd80a5b27
Acer Nitro WSL <mac2x.manos@gmail.com>
2024-11-11 17:40:51 +0800

    completed demo code for race condition

--- OOPROG/MultiThreading/src/RaceConditionDemo.java
+++ OOPROG/MultiThreading/src/RaceConditionDemo.java
@@ -1,58 +1,47 @@
-import java.util.ArrayList;
-
 class RaceCondition implements Runnable {
-    ArrayList<Integer> arrayList;
+    private int count = 0;
 
-    public RaceCondition(ArrayList<Integer> arrayList) {
-        this.arrayList = arrayList;
+    public void increment() {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        count++;
+    }
+
+    public void decrement() {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        count--;
+    }
+
+    public int getCount() {
+        return count;
     }
 
     @Override
     public void run() {
-        Integer value;
-        // This block leads to a race condition
-        value = arrayList.get(2);
-        value = value + 1;
-        arrayList.set(2, value);
-        value = arrayList.get(2);
-        value = value - 2;
-        arrayList.set(2, value);
+        this.increment();
+        System.out.println("Count for thread " + Thread.currentThread().getName() + " after increment: " + this.getCount());
+        this.decrement();
+        System.out.println("Count for thread " + Thread.currentThread().getName() + " after decrement: " + this.getCount());
     }
 }
 
 public class RaceConditionDemo {
     public static void main(String[] args) {
-        ArrayList<Integer> arrList = new ArrayList<>();
+        RaceCondition raceCondition = new RaceCondition();
 
-        arrList.add(1);
-        arrList.add(2);
-        arrList.add(3);
-        arrList.add(4);
-        arrList.add(5);
-
-        System.out.println("Arraylist before multithreading:");
-        System.out.println(arrList);
-
-        RaceCondition raceCondition = new RaceCondition(arrList);
-
-        Thread task1 = new Thread(raceCondition);
-        Thread task2 = new Thread(raceCondition);
-        Thread task3 = new Thread(raceCondition);
+        Thread task1 = new Thread(raceCondition, "Thread 1");
+        Thread task2 = new Thread(raceCondition, "Thread 2");
+        Thread task3 = new Thread(raceCondition, "Thread 3");
 
         task1.start();
         task2.start();
         task3.start();
-
-        System.out.println("ArrayList during multithreading:");
-        System.out.println(arrList);
-
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("ArrayList after multithreading:");
-        System.out.println(arrList);
     }
 }
