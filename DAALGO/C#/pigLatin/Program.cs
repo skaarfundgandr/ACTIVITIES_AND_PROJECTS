a83c6b5259aa70e08859cad4c81b243950a81234
skaarfungandr <mac2x.manos@gmail.com>
2025-01-28 18:42:59 +0800

    Added code for pigLatin in DAALGO using C#

---
+++ DAALGO/C#/pigLatin/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace pigLatin {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter text to translate:");
+
+            Console.WriteLine($"Piglatin: {pigLatin(Console.ReadLine())}");
+        }
+        public static string pigLatin(string src) {
+            char firstLetter = '\0';
+            string res = "";
+
+            for (int idx = 0; idx < src.Length; idx++) {
+                if (idx == 0) {
+                    firstLetter = char.ToLower(src[idx]);
+                } else {
+                    if (src[idx - 1] == ' ' && src[idx] != ' ') {
+                        firstLetter = char.ToLower(src[idx]);
+                    } else if (src[idx - 1] == ' ' && src[idx] == ' ') {
+                        continue;
+                    } else if (src[idx] == ' ' || idx == src.Length - 1) {
+                        res += firstLetter + "ay ";
+                    } else {
+                        res += src[idx];
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
